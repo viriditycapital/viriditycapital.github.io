f806ff6492ac7806a8fea8c50f8c5d5700287e92
mikinty <youmichaelc@gmail.com>
2021-09-11 21:53:34 -0400

    Stripes background viridity logo

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ import React from "react";
 class App extends React.Component {
   render() {
     return (
-      <div id="App">Hello World</div>
+      <div id="App">
+        <div id="background">
+          <div id="stripe-wrapper">
+            <div className="stripe" id="left"></div>
+            <div className="stripe" id="mid"></div>
+            <div className="stripe" id="right"></div>
+          </div>
+        </div>
+      </div>
     );
   }
 }
